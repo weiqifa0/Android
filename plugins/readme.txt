7ff49ad7064f1114f4133b8bb4369d9d38ba8eaa
weiqifa <weiqifa0@qq.com>
2015-11-26 10:17:38 +0800

    [问题描述]:给我们ubuntu下面的gedit安装上查看函数名，变量名的插件 [评审人]:韦启发
    	new file:   plugins/README.markdown
    	new file:   plugins/readme.txt
    	new file:   plugins/sourcecodebrowser.plugin
    	new file:   plugins/sourcecodebrowser/__init__.py
    	new file:   plugins/sourcecodebrowser/ctags.py
    	new file:   plugins/sourcecodebrowser/data/configure_dialog.ui
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/missing-image.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-class.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-code-browser.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-define.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-enumerator.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-field.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-function.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-macro.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-member.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-method.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-namespace.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-property.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-struct.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-table.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-typedef.png
    	new file:   plugins/sourcecodebrowser/data/icons/16x16/source-variable.png
    	new file:   plugins/sourcecodebrowser/data/org.gnome.gedit.plugins.sourcecodebrowser.gschema.xml
    	new file:   plugins/sourcecodebrowser/plugin.py

---
+++ plugins/readme.txt
@@ -0,0 +1,3 @@
+安装步骤
+１、解压后把文件放到~/.local/share/gedit/plugins/目录下面，如果没有这个目录就手动新建
+２、三个文件依次是README.markdown  sourcecodebrowser  sourcecodebrowser.plugin
---
+++ plugins/sourcecodebrowser/__init__.py
@@ -0,0 +1,10 @@
+import sys, os
+
+path = os.path.dirname(__file__)
+
+if not path in sys.path:
+    sys.path.insert(0, path)
+
+import plugin
+from plugin import SourceCodeBrowserPlugin
+
